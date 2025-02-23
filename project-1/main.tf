caacd84ef8bebeb581b5aad73ca9f4ca681192c8
kiranbs2000 <kiranbs6361@gmail.com>
2025-02-23 10:13:00 +0000

    change

---
+++ project-1/main.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+    region = "us-east-1"
+}
+resource "aws_instance" "instance" {
+  ami = "ami-05b10e08d247fb927"
+  instance_type = "t2.micro"
+  security_groups = ["linux-sg"]
+  key_name = "my-ec2-keypair"
+  root_block_device {
+    volume_size = 20
+    volume_type = "gp2"
+    delete_on_termination = "true"
+  }
+  tags = {
+    Name = "terrsform-server"
+  }
+}
